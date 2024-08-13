377d720e371e39e5bf17206442bae52e028ac169
github-roshini <109035217+github-roshini@users.noreply.github.com>
2024-08-13 12:52:20 +0530

    Testing 1

---
+++ index.js
@@ -0,0 +1,6 @@
+/*console.log(`Hello`);
+alert('New Alert');
+alert('who are you?') */
+
+//document.getElementById("Content1").textContent="How Are You?";
+//document.getElementById("myh").textContent = 'Temperature'; 
